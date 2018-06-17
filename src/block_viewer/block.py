0f4371116088d94d9e942b7dd9bd14202fa3a527
Yan Qian <qianyan@thoughtworks.com>
2018-06-17 23:38:29 +0800

    [Refactor] block header test to block test

--- src/block_viewer/block.py
+++ src/block_viewer/block.py
@@ -1,4 +1,5 @@
 from utils import decode_varint
+from block_header import BlockHeader
 
 class Block(object):
     def __init__(self, magic_number=0xD9B4BEF9, block_size=None, block_header=None, tx_counter=None, txs=None):
@@ -11,6 +12,7 @@ class Block(object):
     def parse_from_binary(self, block_data):
         block_header_size = 80
         offset = block_header_size
+        self.block_header = BlockHeader().parse_from_binary(block_data[:offset])
 
         self.tx_counter, varint_size = decode_varint(block_data[offset:])
         offset += varint_size
--- tests/test_block.py
+++ tests/test_block.py
@@ -7,3 +7,17 @@ def test_block_parser():
 
     assert bitcoin_block.magic_number == 3652501241 
     assert bitcoin_block.tx_counter == 1031 
+
+def test_block_header_parser():
+    binary_data = read('tests/fixtures/000000000000000001f942eb4bfa0aeccb6a14c268f4c72d5fff17270da771b9.bin')
+    bitcoin_block = Block().parse_from_binary(binary_data)
+    block_header = bitcoin_block.block_header
+
+    assert block_header.version == 2
+    assert block_header.hash_previous_block == '00000000000000000a3ed9a4e25407518aa854f09fa1981adaae9455a91d1966'
+    assert block_header.hash_merkle_root == '9b7d5896398581a7ff26be4b3684ddd95a7c1dc1aab1df37cbb2127379ae8584'
+    assert block_header.time == 1432723472
+    assert block_header.bits == 404129525
+    assert block_header.nonce == 226994584
+    
+
